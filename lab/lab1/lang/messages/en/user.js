df66a420cfd70161ee935f35c4b13e994d32122d
Jesper H <seonghyun91@gmail.com>
2024-01-19 13:02:00 -0800

    bug fix

--- lab/lab1/lang/messages/en/user.js
+++ lab/lab1/lang/messages/en/user.js
@@ -4,6 +4,9 @@ const messages = {
   add: "Add",
   remove: "Remove",
   goBack: "Go back",
+  readerButton: "Reader",
+  writerButton: "Writer",
+  labTitle: "Lab 1: JSON, Object Constructor, localStorage"
 };
 
 export default messages;
